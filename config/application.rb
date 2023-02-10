fe71e4f031efba82c46b147b68a972d5291c6c05
sea-yassan33 <a.yasuda_tr51@me.com>
2023-02-11 07:04:35 +0900

    ジェネレータ設定

--- config/application.rb
+++ config/application.rb
@@ -2,18 +2,20 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TaskApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.skip_routes true        #routes.rbの変更されず
+      g.helper false            #ヘルパーファイルの生成せず
+      g.assets false            #css,JavaScriptファイル生成せず
+      g.test_framework :rspec   #testファイル生成せず
+      g.controller_specs false  #specs_controller生成せず
+      f.view_specs false        #specs_view生成せず
+    end
   end
 end
